6ac322fb061a994e821310245a6d3d5d71b5952f
SHIVAM SATYAM <shivamsatyam209@gmail.com>
2023-10-26 18:14:07 +0530

    6

--- Routes/route.js
+++ Routes/route.js
@@ -3,10 +3,15 @@ import express from 'express';
 const route =express.Router();
 
 import { addUser,getUsers } from '../Controller/user-controller.js';
-import { newConversation } from '../Controller/conversation-controller.js';
+import { newConversation, getConversation } from '../Controller/conversation-controller.js';
+import { newMessage, getMessages } from '../Controller/message-controller.js';
 
 route.post('/add',addUser);
 route.get('/users', getUsers);
+
 route.post('/conversation/add', newConversation);
+route.post('/conversation/get', getConversation);
+route.post('/message/add', newMessage)
+route.get('/message/get/:id', getMessages);
 
 export default route;
