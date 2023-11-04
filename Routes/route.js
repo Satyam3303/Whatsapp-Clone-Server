d8ca55998c671e403728a70c6ddb1fbdeb6c8f1e
SHIVAM SATYAM <shivamsatyam209@gmail.com>
2023-11-04 13:32:05 +0530

    final commit

--- Routes/route.js
+++ Routes/route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
-const route =express.Router();
 
 import { addUser,getUsers } from '../Controller/user-controller.js';
 import { newConversation, getConversation } from '../Controller/conversation-controller.js';
 import { newMessage, getMessages } from '../Controller/message-controller.js';
+import { uploadFile, getImage } from '../Controller/image-controller.js';
+
+import upload from '../utils/upload.js';
+
+const route =express.Router();
 
 route.post('/add',addUser);
 route.get('/users', getUsers);
@@ -13,5 +17,8 @@ route.post('/conversation/add', newConversation);
 route.post('/conversation/get', getConversation);
 route.post('/message/add', newMessage)
 route.get('/message/get/:id', getMessages);
+
+route.post('/file/upload', upload.single("file"), uploadFile);
+route.get('/file/:filename', getImage);
 
 export default route;
